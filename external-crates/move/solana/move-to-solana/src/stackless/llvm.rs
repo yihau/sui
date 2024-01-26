33fa063a474bd766d56abd24e680311d93cab7ef
Brian Anderson <andersrb@gmail.com>
2024-01-26 17:01:25 +0000

    Add Solana-specific LLVM defs to move-to-solana

--- external-crates/move/solana/move-to-solana/src/stackless/llvm.rs
+++ external-crates/move/solana/move-to-solana/src/stackless/llvm.rs
@@ -42,6 +42,17 @@ use crate::stackless::{
     GlobalContext, ModuleContext,
 };
 
+// These only exist in the Solana LLVM fork,
+// and are not provided by the llvm-sys crate.
+extern "C" {
+    pub fn LLVMInitializeSBFTargetInfo();
+    pub fn LLVMInitializeSBFTarget();
+    pub fn LLVMInitializeSBFTargetMC();
+    pub fn LLVMInitializeSBFAsmPrinter();
+    pub fn LLVMInitializeSBFAsmParser();
+    pub fn LLVMInitializeSBFDisassembler();
+}
+
 pub fn initialize_sbf() {
     unsafe {
         LLVMInitializeSBFTargetInfo();
